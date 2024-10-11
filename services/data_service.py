ae9878c01d09e755f456f76d1573e6654376190b
Филиппов Данил <Filippov_DA@npkvip.ru>
2024-10-11 10:15:25 +0500

    Add data service

---
+++ services/data_service.py
@@ -0,0 +1,28 @@
+from db.models.data_dto import DataDTO
+from db.repositories.data_repository import DataRepository
+from errors.authentication_erros import AuthenticationError
+from services.session_service import SessionService
+from services.user_signatur_service import UserSignatureService
+
+
+class DataService:
+
+    def __init__(self,
+                 session_service: SessionService,
+                 data_repository: DataRepository,
+                 user_signature_service: UserSignatureService) -> None:
+        self.user_signature_service = user_signature_service
+        self.data_repository = data_repository
+        self.session_service = session_service
+
+    def get_data(self,
+                 session_key: str,
+                 key: str,
+                 signature: str) -> DataDTO:
+        session = self.session_service.get_session(session_key)
+        if self.user_signature_service.validate_signature(signature,
+                                                          session.challenge,
+                                                          session.secret):
+            return self.data_repository.load(key)
+
+        raise AuthenticationError("Invalid signature")
