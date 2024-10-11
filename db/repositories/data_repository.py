6561781e85de7f6d7fbb197eaf76c74910b59121
Филиппов Данил <Filippov_DA@npkvip.ru>
2024-10-11 15:44:53 +0500

    Implement data repository

--- db/repositories/data_repository.py
+++ db/repositories/data_repository.py
@@ -1,22 +1,40 @@
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
 from db.models.data_dto import DataDTO
 from db.orm.data_orm import DataModel
 from db.repositories.base import Repository
 from db.session import Session
+from errors.data_errors import NoDataError
 
 
 class DataRepository(Repository[DataModel, DataDTO]):
 
     def save(self, data: DataDTO) -> DataDTO:
-        data_orm = DataModel(key=data.key, data=data.data)
+        data_orm = DataModel(key=data.key,
+                             data=data.data)
         with Session() as session:
             session.add(data_orm)
+            prev_data = session.execute(select(DataModel)
+                                        .where(DataModel.key == data_orm.key)
+                                        ).scalar_one_or_none()
+            if prev_data is None:
+                session.add(data_orm)
+            else:
+                data_orm = session.merge(data_orm)
             session.commit()
-        return data
+            result = self.mapper_to_dto(data_orm)
+            session.commit()
+        return result
 
     def load(self, key: str) -> DataDTO:
-        with Session() as session:
-            data_orm = session.get_one(DataModel, DataModel.key == key)
-        return self.mapper_to_dto(data_orm)
+        try:
+            with Session() as session:
+                data_orm = session.get_one(DataModel, key)
+                result = self.mapper_to_dto(data_orm)
+        except NoResultFound:
+            raise NoDataError("No data found")
+        return result
 
     @staticmethod
     def mapper_to_dto(user_orm: DataModel) -> DataDTO:
