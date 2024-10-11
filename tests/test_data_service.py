c43906c0782f177e911639e378c390bf9235a504
Филиппов Данил <Filippov_DA@npkvip.ru>
2024-10-11 15:45:44 +0500

    Add test to test_data_service.py

---
+++ tests/test_data_service.py
@@ -0,0 +1,30 @@
+import hmac
+
+from sqlalchemy import text
+
+from db.session import engine
+from tests.base import BaseTestCase
+
+
+class TestDataService(BaseTestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        with engine.connect() as conn:
+            conn.execute(text("TRUNCATE TABLE \"data\" RESTART IDENTITY CASCADE;"))
+            conn.commit()
+
+    def test_setting_and_getting_data(self):
+        self.authentication_service.register("test", "test")
+        session_key = self.authentication_service.login("test", "test")
+        challenge = self.authentication_service.get_challenge(session_key)
+
+        secret_key = self.get_secret(session_key)
+
+        signature = hmac.new(secret_key.to_bytes(),
+                             challenge.encode(),
+                             "sha256").hexdigest()
+        msg = 'test'
+        self.data_service.insert(session_key, "test_key", msg, signature)
+        ret_data = self.data_service.get_data(session_key, "test_key", signature)
+        self.assertEqual(msg, ret_data.data)
