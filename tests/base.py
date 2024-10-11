a8666cb720af95b6f9f88e46a1933c7b6531e0f8
Филиппов Данил <Filippov_DA@npkvip.ru>
2024-10-11 15:44:14 +0500

    Add data service to base test case

--- tests/base.py
+++ tests/base.py
@@ -1,16 +1,20 @@
+import secrets
 import unittest
 
 from sqlalchemy import text
 
+from db.repositories.data_repository import DataRepository
 from db.repositories.session_repository import SessionRepository
 from db.repositories.user_repository import UserRepository
 from db.session import engine
 from services.authentication_service import AuthenticationService
 from services.challenge_service import ChallengeService
+from services.data_service import DataService
 from services.dh_service import DHService
 from services.password_service import PasswordService
 from services.session_service import SessionService
 from services.user_service import UserService
+from services.user_signatur_service import UserSignatureService
 
 
 class BaseTestCase(unittest.TestCase):
@@ -32,9 +36,26 @@ class BaseTestCase(unittest.TestCase):
             cls.password_service,
             cls.challenge_service
         )
+        cls.data_repo = DataRepository()
+        cls.user_signature_service = UserSignatureService()
+        cls.data_service = DataService(cls.session_service,
+                                       cls.data_repo,
+                                       cls.user_signature_service)
 
     def tearDown(self):
         with engine.connect() as conn:
             conn.execute(text("TRUNCATE TABLE \"user\" RESTART IDENTITY CASCADE;"))
             conn.execute(text("TRUNCATE TABLE \"session\" RESTART IDENTITY CASCADE;"))
             conn.commit()
+
+    def get_secret(self, session_key):
+        pk = secrets.randbits(16)
+
+        prime, generator = self.authentication_service.get_public_keys()
+
+        our_partial_key = pow(generator, pk, prime)
+
+        server_partial_key = self.authentication_service.get_partial_key(session_key,
+                                                                         our_partial_key)
+
+        return pow(server_partial_key, pk, prime)
