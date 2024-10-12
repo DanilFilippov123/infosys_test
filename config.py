bf3e33509d17ae8bc983ca3e0502fada6f4c12c1
Филиппов Данил <filippok2004180@gmail.com>
2024-10-12 18:26:21 +0500

    Add config option, logging and error handling

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import logging
 import os
 
-database_url = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL", "sqlite://")
 address = ('0.0.0.0', 8000)
+prime = int(os.getenv("PRIME", 23))
+generator = int(os.getenv("GENERATOR", 2))
+LOG_LEVEL = logging.INFO
 
 
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import logging
 from xmlrpc.server import SimpleXMLRPCServer
 
 import config
@@ -12,6 +13,20 @@ from services.password_service import PasswordService
 from services.session_service import SessionService
 from services.user_service import UserService
 from services.user_signatur_service import UserSignatureService
+
+logging.basicConfig(level=config.LOG_LEVEL, format="%(filename)s-%(asctime)s-%(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+
+
+def return_error_on_exception(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.error(e)
+            return f"error: {e}"
+
+    return wrapper
 
 
 class XMLRPCServer:
@@ -60,44 +75,59 @@ class XMLRPCServer:
             self.challenge_service
         )
 
+    @return_error_on_exception
     def register(self,
                  login: str,
                  password: str) -> str:
+        logger.info("Registering user: %s", login)
         return self.authentication_service.register(
             login,
             password
         )
 
+    @return_error_on_exception
     def login(self,
               login: str,
               password: str) -> str:
+        logger.info("Logging in user: %s", login)
         return self.authentication_service.login(
             login,
             password
         )
 
+    @return_error_on_exception
     def logout(self, session_key: str) -> str:
+        logger.info("Logging out user: %s", session_key)
         return self.authentication_service.logout(session_key)
 
+    @return_error_on_exception
     def get_public_keys(self) -> tuple[int, int]:
+        logger.info("Getting public keys")
         return self.authentication_service.get_public_keys()
 
+    @return_error_on_exception
     def get_partial_key(self, session_key: str, partial_key: int) -> int:
+        logger.info("Getting partial key")
         return self.authentication_service.get_partial_key(session_key,
                                                            partial_key)
 
+    @return_error_on_exception
     def get_challenge(self,
                       session_key: str) -> str:
+        logger.info("Getting challenge")
         return self.authentication_service.get_challenge(
             session_key
         )
 
+    @return_error_on_exception
     def get_data(self,
                  session_key: str,
                  key: str,
                  signature: str) -> str:
+        logger.info("Getting data for key %s session: %s", key, session_key)
         return self.data_service.get_data(session_key, key, signature).data
 
+    @return_error_on_exception
     def insert_data(self,
                     session_key: str,
                     key: str,
@@ -114,5 +144,6 @@ server = SimpleXMLRPCServer(
     encoding='ascii'
 )
 
-server.register_instance(XMLRPCServer())
+server.register_instance(XMLRPCServer(prime=config.prime,
+                                      generator=config.generator))
 server.register_introspection_functions()
